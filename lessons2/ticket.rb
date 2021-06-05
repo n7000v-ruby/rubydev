6390da9441a8fff820a1caccf3415493b1608145
Vladislav Afanasiev <n7000v@gmail.com>
2021-06-05 11:44:00 +0300

    домашнее задания 2

---
+++ lessons2/ticket.rb
@@ -0,0 +1,33 @@
+class Ticket
+  def date
+    '04.06.2021'
+  end
+
+  def price
+    3500
+  end
+
+  def row
+    4
+  end
+
+  def seat
+    12
+  end
+
+  def event
+    'Трудно быть рубистом'
+  end
+
+  # TODO
+  # Название фильма
+  # Место
+  # Ряд
+end
+
+ticket = Ticket.new
+p 'Дата фильма: ' + ticket.date
+p 'Стоимость фильма: ' + ticket.price.to_s
+p 'Ряд: ' + ticket.row.to_s
+p 'Место: ' + ticket.seat.to_s
+p 'Мероприятие: ' + ticket.event
