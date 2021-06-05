52fa8f58a43cba181e198638aaf6ef01d0dea983
Vladislav Afanasiev <n7000v@gmail.com>
2021-06-05 13:08:43 +0300

    Классы и объекты

---
+++ lessons2/class.rb
@@ -0,0 +1,25 @@
+#Подобрал имена классов. Создал классы и объекты
+
+class User
+	end
+
+user = User.new
+puts user.class
+
+class Comp
+	end
+
+comp = Comp.new
+puts comp.class
+
+class Net
+	end
+
+net = Net.new
+puts net.class
+
+class Host
+	end
+
+host = Host.new
+puts host.class
