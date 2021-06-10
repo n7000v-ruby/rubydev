fbc23c8436ab9df12cea5ee9dfc0aa171b12c10e
Vladislav Afanasiev <n7000v@gmail.com>
2021-06-10 14:04:10 +0300

    ФИО пользователя через утилиту rdoc

---
+++ lessons3/user.rb
@@ -0,0 +1,33 @@
+##
+# Класс пользователя
+class User
+##
+# Указываем методы ФИО
+  def surname
+    'Фамилия'  
+  end
+
+  def name
+    'Имя'
+  end
+
+  def patronymic
+    'Отчество'
+  end
+
+  def profession
+    'Профессия'
+  end
+
+end
+
+##
+# Создаем новый обьект пользователя
+user = User.new
+
+##
+# Выводим ФИО
+puts user.surname
+puts user.name
+puts user.patronymic
+puts user.profession
