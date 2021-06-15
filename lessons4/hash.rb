00720d295c73ab828355618b835ec96146c90775
Vladislav Afanasiev <n7000v@gmail.com>
2021-06-15 19:28:16 +0300

    Названия цветов радуги

---
+++ lessons4/hash.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+rainbow = {
+  red: 'красный',
+  orange: 'оранжевый',
+  yellow: 'желтый',
+  green: 'зеленый',
+  blue: 'синий',
+  purple: 'фиолетовый'
+}
+
+p rainbow
